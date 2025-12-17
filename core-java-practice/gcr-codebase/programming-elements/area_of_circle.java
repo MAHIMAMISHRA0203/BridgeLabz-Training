8ef4bbccdd4cc369031ff78f8edc3eba3ad33b76
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2025-12-17 16:04:35 +0530

    gcr daY_one

---
+++ core-java-practice/gcr-codebase/programming-elements/area_of_circle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class area_of_circle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double radius = sc.nextInt();
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+    }
+}
