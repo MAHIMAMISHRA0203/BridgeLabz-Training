fe001d96a1a71a89ce3a1bbadcdf5fc008c5e636
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2025-12-20 15:06:45 +0530

    (Mahima Mishra)Refactor. Code of If bonus can be granted or not

--- core-java-practice/gcr-codebase/conditionals/Bonus.java
+++ core-java-practice/gcr-codebase/conditionals/Bonus.java
@@ -1,7 +1,10 @@
 public class Bonus {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter salary");
         int salary = sc.nextInt();
+        System.out.println("Enter years");
+
         int years = sc.nextInt();
         int bonus = 0;
         if (years > 5)
