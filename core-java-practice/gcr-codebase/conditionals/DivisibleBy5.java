d373b13d93179aa06848ecef42f929133c3cc21c
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2025-12-20 15:40:59 +0530

    (Mahima Mishra)Refactor. Code of checking if given input is divisble by 5 or not

--- core-java-practice/gcr-codebase/conditionals/DivisibleBy5.java
+++ core-java-practice/gcr-codebase/conditionals/DivisibleBy5.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class DivisibleBy5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number:");
         int number = sc.nextInt();
-        bool isdiv = number % 5 == 0 ? true : False;
-        System.out.println("Is the number divisible by 5? " + isdiv);
+        bool isDivisible = number % 5 == 0 ? true : False;
+        System.out.println("Is the number divisible by 5? " + isDivisible);
 
     }
 }
