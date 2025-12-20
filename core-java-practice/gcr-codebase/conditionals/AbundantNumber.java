d6bb466ccd02110f0cee055b9c530e7f3d4feb37
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2025-12-20 14:50:49 +0530

    (Mahima Mishra)Refactor. Code of Abundunt Number

--- core-java-practice/gcr-codebase/conditionals/AbundantNumber.java
+++ core-java-practice/gcr-codebase/conditionals/AbundantNumber.java
@@ -1,10 +1,9 @@
-public class abundantNumber {
+public class AbundantNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        int i = 0;
         int sum = 0;
-        while (i > 0) {
+        while (number > 0) {
             int temp = number % 10;
             number /= 10;
             sum += temp;
