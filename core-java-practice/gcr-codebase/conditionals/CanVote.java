30bec85610baba1628ed4bf28745a5ed94440da2
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2025-12-20 15:11:47 +0530

    (Mahima Mishra)Refactor. Code of If a peroon can vote or not

--- core-java-practice/gcr-codebase/conditionals/CanVote.java
+++ core-java-practice/gcr-codebase/conditionals/CanVote.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class CanVote {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-        if (age > 18)
-            System.out.printf("The persons age is  %d and can vote");
+        System.out.println("Enter  your age");
+
+        int yourAge = sc.nextInt();
+        if (yourAge > 18)
+            System.out.printf("The persons age is  %d and can vote", yourAge);
         else {
-            System.out.printf("The persons age is  %d and can't vote");
+            System.out.printf("The persons age is  %d and can't vote", yourAge);
         }
     }
 }
