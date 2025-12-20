1714d7f782145f1aaeb2e5be6b67b6746475fd66
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2025-12-20 15:06:16 +0530

    (Mahima Mishra)Refactor. Code of If bonus can be granted or not

--- core-java-practice/gcr-codebase/conditionals/BMI.java
+++ core-java-practice/gcr-codebase/conditionals/BMI.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class BMI {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("please enter weight");
         double weight = sc.nextDouble();
+        System.out.println("please enter height");
+
         double height = sc.nextDouble();
         double BodyMassIndex = weight / (height * height);
         if (BodyMassIndex <= 18.4) {
