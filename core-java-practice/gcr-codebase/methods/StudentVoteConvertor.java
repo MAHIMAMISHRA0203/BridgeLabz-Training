f0bb3b0c62d73e54253719ab6c0faba0f82ca90e
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2025-12-24 16:20:48 +0530

    (Mahima Mishra) Code to check if a person can vote or not

---
+++ core-java-practice/gcr-codebase/methods/StudentVoteConvertor.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StudentVoteConvertor {
+
+    static boolean canStudentVote(int age) {
+        if (age < 0)
+            return false;
+        return age >= 18;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Enter age of student " + i + ": ");
+            int age = sc.nextInt();
+
+            if (canStudentVote(age))
+                System.out.println("Can Vote");
+            else
+                System.out.println("Cannot Vote");
+        }
+    }
+}
