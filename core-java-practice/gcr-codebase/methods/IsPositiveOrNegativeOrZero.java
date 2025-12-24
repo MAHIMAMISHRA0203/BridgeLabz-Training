14b527b59e45ab9c572f89d0bfb8bea75ed3c65a
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2025-12-24 11:52:16 +0530

    (Mahima Mishra) Code to check if a number is positive o negative or zero

--- core-java-practice/gcr-codebase/methods/IsPositiveOrNegativeOrZero.java
+++ core-java-practice/gcr-codebase/methods/IsPositiveOrNegativeOrZero.java
@@ -10,6 +10,7 @@ public class IsPositiveOrNegativeOrZero {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
         int number = sc.nextInt();
         System.out.println(positiveOregativeOrZero(number));
 
