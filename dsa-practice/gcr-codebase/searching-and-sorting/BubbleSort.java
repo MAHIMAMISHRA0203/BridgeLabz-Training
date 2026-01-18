092e464ae33debcf05a86ed05e5558befa7a2a75
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-01-18 12:12:28 +0530

    (Mahima Mishra) Add. Code to find first negative number using linear search

---
+++ dsa-practice/gcr-codebase/searching-and-sorting/BubbleSort.java
@@ -0,0 +1,23 @@
+package dsa_practice.gcr_codebase;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        // input array
+        int arr[] = { 3, 67, 9, 2, 90 };
+        // for loop to sort
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                // checking if left element is greater than right if yes then swap
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        // displaying element
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
