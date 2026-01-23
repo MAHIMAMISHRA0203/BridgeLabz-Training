6553272b8ff5739e02e7719bfaf9b5c467484860
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-01-23 19:43:44 +0530

    (Mahima Mishra) Add. Code to find nth element from end

---
+++ gcr-codebase/collections/NthElement.java
@@ -0,0 +1,10 @@
+package collections;
+
+public class NthElement {
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 8, 1 };
+        int n = 2;
+        System.out.println(arr[arr.length - n]);
+    }
+
+}
