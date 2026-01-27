e2ac9015487907888d9b12e06822870cdde0daab
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-01-27 16:32:58 +0530

    (Mahima Mishra)Add. Code to use unchecked exception by division of two numbers

---
+++ gcr-codebase/exceptions/uncheckedException.java
@@ -0,0 +1,21 @@
+package Exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class uncheckedException {
+public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+
+	try {
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a/b;
+		System.out.println(c);
+	}catch(ArithmeticException e) {
+		System.out.println(e);
+	}catch(InputMismatchException e) {
+		System.out.println(e);
+	}
+}
+}
