745b15da5e6c3be4d8fcef1915827c9fa250fc6d
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-02-02 21:14:50 +0530

    (Mahima Mishra) Add . Code to deprecated annotation use

--- gcr-codebase/annotations/Overriding.java
+++ gcr-codebase/annotations/Overriding.java
@@ -1,5 +1,22 @@
 package annotations;
 
-public class Overriding {
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
 
+class Dog extends Animal {
+
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Overriding {
+    public static void main(String[] args) {
+        Animal a = new Dog(); // upcasting
+        a.makeSound();
+    }
 }
