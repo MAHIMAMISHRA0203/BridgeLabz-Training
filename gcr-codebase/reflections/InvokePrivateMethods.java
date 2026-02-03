462f5e18eb454ddb7936ce4857f2f4b027cca8de
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-02-03 15:47:51 +0530

    (Mahima Mishra) ADD. Code to dynamically create objects

--- gcr-codebase/reflections/InvokePrivateMethods.java
+++ gcr-codebase/reflections/InvokePrivateMethods.java
@@ -1,7 +1,19 @@
 package reflections;
 
+import java.lang.reflect.Method;
+
 public class InvokePrivateMethods {
 
+    public static void main(String[] args) throws Exception {
+
+        Calculator calc = new Calculator();
+        Method method = Calculator.class.getDeclaredMethod("multiply", int.class, int.class);
+
+        method.setAccessible(true);
+        Object result = method.invoke(calc, 5, 4);
+
+        System.out.println("Result: " + result);
+    }
 }
 
 class Calculator {
