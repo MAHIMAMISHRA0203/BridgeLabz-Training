7e265c888e54cdc3c776d484e301cbcb444acac4
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-02-03 15:46:47 +0530

    (Mahima Mishra) ADD. Code to invoke private methods

---
+++ gcr-codebase/reflections/InvokePrivateMethods.java
@@ -0,0 +1,11 @@
+package reflections;
+
+public class InvokePrivateMethods {
+
+}
+
+class Calculator {
+    private int multiply(int a, int b) {
+        return a * b;
+    }
+}
--- gcr-codebase/reflections/ModifyPrivateFields.java
+++ gcr-codebase/reflections/ModifyPrivateFields.java
@@ -1,9 +1,21 @@
 package reflections;
 
-class Person {
-    private int age = 20;
-}
+import java.lang.reflect.Field;
 
 public class ModifyPrivateFields {
 
+    public static void main(String[] args) throws Exception {
+
+        Person p = new Person();
+        Field field = Person.class.getDeclaredField("age");
+
+        field.setAccessible(true);
+        field.set(p, 30);
+
+        System.out.println("Age: " + field.get(p));
+    }
+}
+
+class Person {
+    private int age = 20;
 }
