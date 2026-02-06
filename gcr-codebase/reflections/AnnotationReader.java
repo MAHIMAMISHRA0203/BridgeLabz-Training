11a288459389b29dd1d83dbcdca7b0831bf6a9eb
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-02-06 13:16:09 +0530

    (Mahima Mishra) Add . Code to read annoattions at runtime

---
+++ gcr-codebase/reflections/AnnotationReader.java
@@ -0,0 +1,24 @@
+package reflections;
+
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface Author {
+    String name();
+}
+
+@Author(name = "Mahima")
+class Book {
+}
+
+public class AnnotationReader {
+
+    public static void main(String[] args) {
+        Class<Book> cls = Book.class;
+
+        Author author = cls.getAnnotation(Author.class);
+
+        System.out.println("Author:" + author.name());
+    }
+}
