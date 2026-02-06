5821e7ab062fedc2b4b301c9c971d2fb2c577669
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-02-06 12:24:24 +0530

    (Mahima Mishra) Add . Code to Define an annotation @JsonField to mark fields for JSON serialization

---
+++ gcr-codebase/annotations/ImportMethodDemo.java
@@ -0,0 +1,34 @@
+package annotations;
+
+import java.lang.annotation.*;
+import java.lang.reflect.Method;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface ImportantMethod {
+    String level() default "HIGH";
+}
+
+class SystemTasks {
+    @ImportantMethod
+    public void backup() {
+        System.out.println("Backup Done");
+    }
+
+    @ImportantMethod(level = "LOW")
+    public void cleanTemp() {
+        System.out.println("Temp Cleaned");
+    }
+}
+
+public class ImportMethodDemo {
+    public static void main(String[] args) {
+        Method[] methods = SystemTasks.class.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(ImportantMethod.class)) {
+                ImportantMethod info = method.getAnnotation(ImportantMethod.class);
+                System.out.println("Method:" + method.getName() + " Level:" + info.level());
+            }
+        }
+    }
+}
