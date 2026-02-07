4ea0f2a7c54b90ee92c22f55ebfad1d4f6e3a823
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-02-07 15:58:26 +0530

    (Mahma Mishra) Add . Code to use static method in interface using PasswordString Validator

---
+++ gcr-codebase/functionalInterface/PasswordStrenghValidator/Main.java
@@ -0,0 +1,11 @@
+package InterfacesFunctional.PasswordStrenghValidator;
+
+
+public class Main {
+public static void main(String args[]) {
+	String password="re453";
+
+System.out.println(SecurityUtils.isStrong(password));
+	
+}
+}
