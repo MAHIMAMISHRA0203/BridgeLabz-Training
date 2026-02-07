5b34b5a8c95471e74db0af6e61eeae6b10ebbc9a
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-02-07 21:38:10 +0530

    (Mahma Mishra) Add . Code to find top 5movies on from movie list

---
+++ gcr-codebase/StreamAPI/TrendingMoviesAPI/TrendingMovies.java
@@ -0,0 +1,13 @@
+package StreamAPI.TrendingMoviesAPI;
+
+class TrendingMovie {
+    String name;
+    double rating;
+    int releaseYear;
+
+    TrendingMovie(String name, double rating, int releaseYear) {
+        this.name = name;
+        this.rating = rating;
+        this.releaseYear = releaseYear;
+    }
+}
