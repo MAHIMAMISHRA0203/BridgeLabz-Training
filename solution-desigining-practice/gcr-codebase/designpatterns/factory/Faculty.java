da3b69ffa19ccb0b563e9f902ff0abd1dd7895dd
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-03-31 11:53:07 +0530

    Added  module separately

---
+++ solution-desigining-practice/gcr-codebase/designpatterns/factory/Faculty.java
@@ -0,0 +1,7 @@
+package designpatterns.factory;
+
+public class Faculty implements User {
+    public void showRole() {
+        System.out.println("User is Faculty");
+    }
+}
---
+++ solution-desigining-practice/gcr-codebase/designpatterns/factory/Librarian.java
@@ -0,0 +1,8 @@
+package designpatterns.factory;
+
+package factory;
+public class Librarian implements User {
+    public void showRole() {
+        System.out.println("User is Librarian");
+    }
+}
